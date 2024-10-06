dffb8ced3cc4dfa127a5da6e35d4d9e79f24dfbf
codersunny812 <sengersunny448@gmail.com>
2024-10-06 19:19:44 +0530

    bg changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,7 @@ const Hero = () => {
 
         </div>
       </div>
-      <div className="sliderContainer md:z-[-10]">
+      <div className="sliderContainer md:z-[-10] top-24">
         <div ref={slider} className="slider overflow-hidden text-secondary text-[70px] lg:text-[200px]">
           <p ref={firstText} >Full-Stack Developer.</p>
           <p ref={secondText}>Full-Stack Developer.</p>
@@ -68,26 +68,10 @@ const Hero = () => {
       <Lottie
         animationData={heroAnim}
         loop={true}
-        className="z-[-2] absolute bottom-24 right-0 animation"
+        className="z-[-2] absolute bottom-32 right-0 animation"
       />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-30 cursor-pointer'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 cursor-pointer'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
+  
     </section>
   );
 };
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -12,49 +12,50 @@ import Skills from "./Skills";
 const Tech = () => {
   return (
     <>
-    <div className='relative'>
-     <motion.div variants={textVariant()}>
-        <p className={'sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider'}>Tech</p>
-        <h2 className={'text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'}>Skills.</h2>
-      </motion.div>
-      <Image src={color_sharp} alt="color-sharp" className="absolute z-[-1] h-80 -left-60 w-screen -top-20" />
+      <div className='relative'>
+        <motion.div variants={textVariant()}>
+          <p className={'sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider'}>Tech</p>
+          <h2 className={'text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'}>Skills.</h2>
+        </motion.div>
+        <Image src={color_sharp} alt="color-sharp" className="absolute z-[-1] h-80 -left-60 w-screen -top-20" />
       </div>
-    <div className='flex relative z-10 flex-col flex-wrap justify-center gap-10'>
-      <div className=" flex flex-wrap gap-10 justify-center">
-      {technologies.slice(0,7).map((technology,index) => (
-        <div className='w-28 h-28 mt-5' key={technology.name}>
-          <Skills name={technology.name} icon={technology.icon} index={index} />
+      <div className='flex relative z-10 flex-col flex-wrap justify-center gap-10'>
+        <div className=" flex flex-wrap gap-10 justify-center">
+          {technologies.slice(0, 7).map((technology, index) => (
+            <div className='w-28 h-28 mt-5' key={technology.name}>
+              <Skills name={technology.name} icon={technology.icon} index={index} />
+            </div>
+          ))}
         </div>
-      ))}
-      </div>
-      <div className=" flex flex-wrap gap-10 justify-center">
-      {technologies.slice(7,12).map((technology,index) => (
-        <div className='w-28 h-28 mt-5' key={technology.name}>
-          <Skills name={technology.name} icon={technology.icon} index={index} />
+        <div className=" flex flex-wrap gap-10 justify-center">
+          {technologies.slice(7, 12).map((technology, index) => (
+            <div className='w-28 h-28 mt-5' key={technology.name}>
+              <Skills name={technology.name} icon={technology.icon} index={index} />
+            </div>
+          ))}
         </div>
-      ))}
-      </div>
-      <div className=" flex flex-wrap gap-10 justify-center">
-      {technologies.slice(12).map((technology,index) => (
-        <div className='w-24 h-24 mt-5' key={technology.name}>
-          <Skills name={technology.name} icon={technology.icon} index={index} />
+        <div className=" flex flex-wrap gap-10 justify-center">
+          {technologies.slice(12).map((technology, index) => (
+            <div className='w-24 h-24 mt-5' key={technology.name}>
+              <Skills name={technology.name} icon={technology.icon} index={index} />
+            </div>
+          ))}
         </div>
-      ))}
       </div>
-    </div>
-    <div className=" w-full h-full  absolute top-10 flex flex-col  items-center  justify-center">
-        <div className=" w-[1000px] 1000:w-full h-full z-[-10] opacity-[0.3]  absolute flex flex-col bg-cover items-center justify-center ">
+      <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center">
+        <div className="w-full h-full z-[-10] opacity-[0.3] absolute flex flex-col items-center justify-center bg-cover">
           <video
-            className="w-full h-auto"
+            className="w-full h-full object-cover"
             preload="false"
             playsInline
             loop
             muted
             autoPlay="autoplay"
-            src="/cards-video.webm"
+            src="/galaxy-anim.mp4"
           ></video>
         </div>
       </div>
+
     </>
   );
 };
